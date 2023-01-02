ffe19d6d1ff3009e792c1e831cfe3e067a7125c4
Almedin Hodzic <almedin.json@gmail.com>
2023-01-02 22:52:01 +0100

    Added post and get methods for /decks endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import Deck from "./models/Deck";
+import { config } from "dotenv";
+import cors from "cors";
 
+config();
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Home1");
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await Deck.find();
+  res.json(decks);
 });
 
-app.listen(5000, () => console.log("Running on port 5000"));
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({ title: req.body.title });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+mongoose
+  .connect(process.env.MONGO_URI!)
+  .then(() => app.listen(PORT, () => console.log(`Running on port ${PORT}`)));
